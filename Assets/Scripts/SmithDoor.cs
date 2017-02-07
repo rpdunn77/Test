4a924e0a60e0cd1967719ce2f1001d5826294709
rpdunn77 <rp.dunn@yahoo.ca>
2017-02-06 20:09:03 -0800

    accidentally removed files and altered SmithDoor
    accidentally removed files, they are back in, and also altered SmithDoor
    to remove walking down to enter door glitch.

--- Assets/Scripts/SmithDoor.cs
+++ Assets/Scripts/SmithDoor.cs
@@ -4,16 +4,20 @@ using System.Collections;
 public class SmithDoor : MonoBehaviour {
 	public Transform exit;
 	public GameObject newcamera;
+	public bool up;
+	public bool down;
 
 
 	void OnTriggerStay2D(Collider2D other)
 	{
 
-		if (other.GetComponent<Collider2D>().tag == "Player" && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))){
+		if (other.GetComponent<Collider2D>().tag == "Player"){
+			if((up == true && Input.GetKey(KeyCode.W)) || (down == true && Input.GetKey(KeyCode.S))){
 	
-			CameraController.changeCamera (newcamera);
+				CameraController.changeCamera (newcamera);
 
-			other.transform.position = exit.transform.position;
+				other.transform.position = exit.transform.position;
+			}
 		}
 
 	}
